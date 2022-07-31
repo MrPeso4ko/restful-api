a159561dcaa3f3cd26ad367df87ffe7caf356ba9
Petr Salnikov <pdsalnikov@edu.hse.ru>
2022-08-01 01:08:11 +0300

    Implement diploma/list

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Diploma struct {
+	Id      int    `json:"id"`
+	Year    int    `json:"year"`
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+}
+
+var diplomas = []Diploma{{1, 2022, "Petr", "Salnikov"}}
+var diplomas_map = map[int]*Diploma{1: &diplomas[0]}
+
+func get_diplomas(c *gin.Context) { // TODO: optimize
+	var diplomas_year []Diploma
+	for i := 0; i < len(diplomas); i++ {
+		if diplomas[i].Year == 2022 { // TODO: year
+			diplomas_year = append(diplomas_year, diplomas[i])
+		}
+	}
+	c.IndentedJSON(http.StatusOK, diplomas_year)
+}
+
 func main() {
-	$END$
+	router := gin.Default()
+	router.GET("/diploma/list", get_diplomas)
+
+	err := router.Run("localhost:8080")
+	if err != nil {
+		return
+	}
 }
